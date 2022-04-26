b43f5e28e252a326897738a86368ef7bf6fde92e
Erna Lukac <e.lukac4@gmail.com>
2022-04-25 22:20:33 -0600

    added some formating

--- src/teamProfile.js
+++ src/teamProfile.js
@@ -5,12 +5,19 @@ teamWebsite = (teamProfile) => {
     <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Test</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="../src/style.css"/>
+        <title>Team Profile Generator</title>
     </head>
     <body>
-        ${teamMembers}
+        <div class="header col-12">
+            <h1> My Team </h1>
+        </div>
+        <div class="row myTeamCards">
+            ${teamMembers}
+        </div>    
+
     </body>
     </html>
 
@@ -20,17 +27,76 @@ teamWebsite = (teamProfile) => {
 
 }
 
-    teamEmployees = (teamProfile) => {
-    teamProfile.forEach(i=>{
-       role=i.role;
-        `
-        <div>
-            ${role}
-        </div>
-
-        `;
-    }) 
-    return teamEmployees;
+teamEmployees = (teamProfile) => {
+    let finalHtml = '';
+    teamProfile.forEach(i => {
+        role = i.role;
+        name = i.name;
+        id = i.id;
+        email = i.email;
+        if (role == 'Manager') {
+            officeNo = i.officeNo;
+            managerCard = `
+            <div class="card col-4 px-4 py-2">
+            <div class="card-body">
+            <div class="cardTitle">
+                <h4 class="card-title">${name}</h4>
+                <h5 class="card-subtitle mb-2">${role}</h5>
+            </div>  
+              <div class="card">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"> ID: ${id}</li>
+                    <li class="list-group-item">Email: ${email}</li>
+                    <li class="list-group-item">Office Number: ${officeNo}</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+            `;
+            finalHtml += managerCard;
+        } else if (role == 'Engineer') {
+            engineerGitHub = i.engineerGitHub;
+            engineerCard = `
+            <div class="card col-4" style="width: 18rem;">
+            <div class="card-body">
+            <div class="cardTitle">
+                <h4 class="card-title">${name}</h4>
+                <h5 class="card-subtitle mb-2">${role}</h5>
+            </div>  
+              <div class="card">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"> ID: ${id}</li>
+                    <li class="list-group-item">Email: ${email}</li>
+                    <li class="list-group-item">GitHub: ${engineerGitHub}</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+            `;
+            finalHtml += engineerCard;
+        } else {
+            internSchool = i.internSchool;
+            internCard = `
+            <div class="card col-4" style="width: 18rem;">
+            <div class="card-body">
+            <div class="cardTitle">
+                <h4 class="card-title">${name}</h4>
+                <h5 class="card-subtitle mb-2">${role}</h5>
+            </div>  
+              <div class="card">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"> ID: ${id}</li>
+                    <li class="list-group-item">Email: ${email}</li>
+                    <li class="list-group-item">School: ${internSchool}</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+            `;
+            finalHtml += internCard;
+        }
+    })
+    return finalHtml;
 }
 
 
